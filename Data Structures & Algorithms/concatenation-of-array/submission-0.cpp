34b6e9b45ef31328d0ac97f4acb7fb529998465d
diya srivastava <diya.codesdev@gmail.com>
2026-07-01 23:58:14 +0530

    Bulk sync: 6 submissions

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-0.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        vector<int> ans;
+        for(int i=0;i<2;i++){
+            for(int i:nums) ans.push_back(i);
+        }
+        return ans;
+    }
+};
