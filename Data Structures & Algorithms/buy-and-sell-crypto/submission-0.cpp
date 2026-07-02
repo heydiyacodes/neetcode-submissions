97af39210b133806f8fcaed7499ee275bf5866b7
diya srivastava <diya.codesdev@gmail.com>
2026-07-02 07:45:30 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int buy=INT_MAX;
+        int sell=0;
+
+        for(int i=0;i<prices.size();i++){
+            buy=min(prices[i],buy);
+            sell=max(prices[i]-buy,sell);
+        }
+        return sell;
+    }
+};
